e52c5028f836b43d287d807b7d419fd681c654ef
amq <amq@users.noreply.github.com>
2016-05-01 18:07:01 +0200

    Add key generation

--- src/shared.c
+++ src/shared.c
@@ -1,1 +1,16 @@
 #include "shared.h"
+
+key_t shm_key = 0;
+key_t sem_r_key = 1;
+key_t sem_w_key = 2;
+
+/* dynamic key generation */
+void generate_keys(void) {
+    shm_key = 1000 * getuid() + shm_key;
+    sem_r_key = 1000 * getuid() + sem_r_key;
+    sem_w_key = 1000 * getuid() + sem_w_key;
+}
+
+void init(void) {
+    generate_keys();
+}
--- src/shared.h
+++ src/shared.h
@@ -1,6 +1,13 @@
 #ifndef _SENDER_RECEIVER_H_
 #define _SENDER_RECEIVER_H_
 
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 #include "../lib/sem182/sem182.h"
+
+void generate_keys(void);
+void init(void);
 
 #endif /* _SENDER_RECEIVER_H_ */
